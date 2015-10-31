95d997554e7a79ca04af0a5996c387274e679d59
Eduardo Corzo <edukorzo@gmail.com>
2015-10-31 12:01:43 +0100

    Set temperature in html

--- termostaatti/static/js/script.js
+++ termostaatti/static/js/script.js
@@ -3,7 +3,7 @@ appname.controller('appCtrl', ['$scope','$http',
   function($scope, get) {
      get({method: 'GET',url: '/temperatura'})
      .then(function successCallback(response) {
-            $scope.greeting = response
+            $scope.greeting = response.data.Temperature
         }, function errorCallback(response) {
             $scope.greeting ={ text: 'Hello' };
         });
