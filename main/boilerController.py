bf950d5331a32dfc079102a87971bd530fd5b3d3
Eduardo Corzo <edukorzo@gmail.com>
2015-10-31 10:35:24 +0100

    Renaming imports

--- main/boilerController.py
+++ main/boilerController.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from main.boiler import boiler
+from main.boiler import Boiler
 
 @api_view(['GET'])
 def check(request):
--- main/thermometterController.py
+++ main/thermometterController.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from main.thermometter import thermometter
+from main.thermometter import Thermometter
 
 @api_view(['GET'])
 def getTemperature(request):
