a49e718bed59dc6869a706586f5283ad0eac237a
Eduardo Corzo <edukorzo@gmail.com>
2015-11-01 12:58:48 +0100

    Adding library to controller

--- main/boilerController.py
+++ main/boilerController.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from django.utils.datastructures import MultiValueDictKeyError
 from main.boiler import Boiler
 
 @api_view(['GET'])
