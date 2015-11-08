47b50d165bf167cd84ede21c291e3a3dcd3eb751
Eduardo Corzo <edukorzo@gmail.com>
2015-11-08 18:24:30 +0100

    Cleaning html and getting the state of the boiler

--- main/boiler.py
+++ main/boiler.py
@@ -4,6 +4,7 @@ import time
 
 
 class Boiler:
+    
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(12, GPIO.OUT)
@@ -13,3 +14,6 @@ class Boiler:
     
     def turnOff(self):
         GPIO.output(12,False)
+
+    def getState(self):
+        return GPIO.input(12)
