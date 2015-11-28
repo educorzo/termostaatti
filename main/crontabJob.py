ea0c1337eec2b3049693a4491af9c1192c73cac7
Eduardo Corzo <edukorzo@gmail.com>
2015-11-28 12:30:35 +0100

    Check temperature crontab

--- main/crontabJob.py
+++ main/crontabJob.py
@@ -2,5 +2,6 @@
 from crontab import CronTab
 
 cron = CronTab()
-job = cron.new(command='script.py')
+job = cron.new(command='sudo /usr/bin/python /home/pi/termostaatti/main/script.py 10')
 job.minute.every(1)
+cron.write()
--- main/script.py
+++ main/script.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 from thermometter import Thermometter
 from boiler import Boiler
+import sys
 
 boiler = Boiler()
-if boiler.getState() :
-    boiler.turnOff()
+thermometter = Thermometter()
+themperature = sys.argv[1]
+if thermometter.getTemp() < themperature:
+    if !boiler.getState() :
+        boiler.turnOn()
 else:
-    boiler.turnOn()
+    boiler.turnOff()
+
 
